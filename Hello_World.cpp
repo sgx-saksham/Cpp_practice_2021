684a7e90a0039427e33c256abbb4d882e885b33e
sgx-saksham <saksham6325@gmail.com>
2021-01-30 21:03:31 +0530

    previously added

---
+++ Hello_World.cpp
@@ -0,0 +1,8 @@
+// First program as expected  "Hello World"
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World";
+    return 0;
+}
---
+++ datatypes.cpp
@@ -0,0 +1,22 @@
+// here i will talk about datatypes in cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+    // integer type
+    int integer = 9;
+    cout << "The value of integer is " << integer << endl;
+    // floating point
+    float floating = 9.555;
+    cout << "The Value of float is " << floating << endl;
+    // String type
+    string sentence = "this is a datatype";
+    cout << "The string is : "
+         << ""
+            " << sentence << "
+            ""
+         << endl;
+    //
+    int *p;
+    *p = 85;
+}
