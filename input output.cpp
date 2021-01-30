e0423973451bbce89e1da9a1f4fd921d8756aa0c
sgx-saksham <saksham6325@gmail.com>
2021-01-30 21:01:21 +0530

    Basic Input Output in cpp

---
+++ input output.cpp
@@ -0,0 +1,10 @@
+// basic input output in cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << "enter an Integer : ";
+    cin >> i;
+    cout << "Your input was : " << i;
+}
