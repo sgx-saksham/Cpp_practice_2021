228543dd99bce1ee6944a0fe99ca6d62e63c0b43
sgx-saksham <saksham6325@gmail.com>
2021-02-11 02:27:02 +0530

    return reference in cpp

---
+++ return_reference.cpp
@@ -0,0 +1,23 @@
+// return reference in cpp
+#include <iostream>
+using namespace std;
+
+// Global variable
+int num;
+
+// Function declaration
+int &test();
+
+int main()
+{
+    test() = 5;
+
+    cout << num;
+
+    return 0;
+}
+
+int &test()
+{
+    return num;
+}
