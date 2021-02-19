f11fec23949acda3870491bec5d081052b48cd31
sgx-saksham <saksham6325@gmail.com>
2021-02-20 00:05:04 +0530

    Enumeration in cpp

---
+++ Enumeration.cpp
@@ -0,0 +1,28 @@
+// Enumeration in CPP
+/*
+ enum boolean 
+{ 
+   false, true
+} check;
+*/
+#include <iostream>
+using namespace std;
+
+enum week
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+};
+
+int main()
+{
+    week today;
+    today = Wednesday;
+    cout << "Day " << today + 1;
+    return 0;
+}
