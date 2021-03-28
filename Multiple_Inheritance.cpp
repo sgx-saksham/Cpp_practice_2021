6eaafffe3ecf0b3ff37cd1b2296cc04aee7bf9d9
sgx-saksham <saksham6325@gmail.com>
2021-03-28 23:56:02 +0530

    Multiple Inheritance

---
+++ Multiple_Inheritance.cpp
@@ -0,0 +1,35 @@
+/* 
+    Saksham is on the Code 
+*/
+// 'Multiple_Inheritance.cpp'
+
+#include <iostream>
+using namespace std;
+
+class Mammal
+{
+public:
+    Mammal()
+    {
+        cout << "Mammals can give direct birth." << endl;
+    }
+};
+
+class WingedAnimal
+{
+public:
+    WingedAnimal()
+    {
+        cout << "Winged animal can flap." << endl;
+    }
+};
+
+class Bat : public Mammal, public WingedAnimal
+{
+};
+
+int main()
+{
+    Bat b1;
+    return 0;
+}
